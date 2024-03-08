7af7e58641d8b3923739c9d8d177071bebf19261
Ivan Ovsyannikov <vanek10ovsannikov@gmail.com>
2024-03-09 00:33:35 +0300

    initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-### FW, Libs: 
-- [ChakraUI](https://chakra-ui.com/)
-- [ReactJS](https://react.dev/)
-- [Vite](https://vitejs.dev/)
-- [Redux](https://redux.js.org/)
--- src/routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const PrivateRoute = () => {
-    return localStorage.getItem("token") ? <Outlet/> : <Navigate to={"/"} replace/>
-}
-
-export default PrivateRoute;
